2c2996926814a03ed5b1cc215c164bf0e3dc5ad8
shestakovitch <shestakovitch@gmail.com>
2025-07-12 18:06:43 +0200

    Added serializer, RunViewSet and router

--- app_run/views.py
+++ app_run/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
+from .models import Run
+from .serializers import RunSerializer
 
 
 @api_view(['GET'])
@@ -11,3 +13,8 @@ def company_details(request):
                'contacts': settings.CONTACTS}
 
     return Response(details)
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
--- project_run/urls.py
+++ project_run/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from app_run.views import company_details
+from rest_framework.routers import DefaultRouter
+from app_run.views import company_details, RunViewSet
+
+router = DefaultRouter()
+router.register('api/runs', RunViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/company_details/', company_details)
+    path('api/company_details/', company_details),
+    path('', include(router.urls))
 ]
