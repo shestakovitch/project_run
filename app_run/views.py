4f47c0882de02583708a64ae05fcb3f5cec00c5b
shestakovitch <shestakovitch@gmail.com>
2025-07-28 14:59:52 +0200

    Added import Min, Max

--- app_run/views.py
+++ app_run/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.views import APIView
 from geopy.distance import geodesic
-from django.db.models import Sum
+from django.db.models import Sum, Min, Max
 import openpyxl
 from rest_framework.exceptions import PermissionDenied
 
